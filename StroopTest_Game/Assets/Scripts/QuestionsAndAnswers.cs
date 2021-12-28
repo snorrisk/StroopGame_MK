41ca06cc932b46689048f0fe1d029e6d6839d7ce
snorrisk <96678347+snorrisk@users.noreply.github.com>
2021-12-28 23:55:30 +1030

    Compiler Error
    A compiler error that I can't rectify is hindering opening the project file.

---
+++ StroopTest_Game/Assets/Scripts/QuestionsAndAnswers.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+[System.Serializable]
+
+public class QuestionsAndAnswers : MonoBehaviour
+{
+
+    // Creating a class for the Quiz Manager
+
+    public string Questions;
+    // Array
+    public string[] Answers;
+    public int CorrectAnswer;
+}
